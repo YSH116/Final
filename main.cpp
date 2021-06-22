6dcf65ccf6ee291bc803425714b6e0d4997c2cae
YSH116 <74852896+YSH116@users.noreply.github.com>
2021-06-22 21:15:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,40 @@
 Ticker servo_ticker;
 
 PwmOut pin5(D5), pin6(D6);
-// DigitalInOut pin11(D11);
-BufferedSerial uart(D10, D9);
-BufferedSerial xbee(D1, D0);
+DigitalInOut pin10(D11);
+BufferedSerial uart(D1, D0);
+BufferedSerial xbee(D10, D9);
 BBCar car(pin5, pin6, servo_ticker);
 
 int main() {
     uart.set_baud(9600);
+    xbee.set_baud(9600);
     char buf[256], outbuf[256];
     FILE *devin = fdopen(&uart, "r");
     FILE *devout = fdopen(&uart, "w");
+    bool round = 1;
+    bool ok = 1;
+    parallax_ping  ping1(pin10);
 
     while (1) {
-        // parallax_ping  ping(pin11);
-        // printf("dis = %f\n", (float)ping);
-        
+        printf("dis = %f\n", (float)ping1);
+        if ((float)ping1 < 15 && round  && (float)ping1 > 5) {
+            car.turn(100, -0.01);
+            ThisThread::sleep_for(1000ms);
+            car.goStraight(100);
+            ThisThread::sleep_for(30ms);
+            car.turn(100, 0.35);
+            ThisThread::sleep_for(6000ms);
+            round = 0;
+            car.stop();
+        }
         memset(buf, 0, 256);
         for( int i = 0; ; i++ ) {
             char recv = fgetc(devin);
             buf[i] = fputc(recv, devout);
-            if (buf[0] == 'K') {
+            if (buf[0] == 'K' && ok) {
                 xbee.write("K\n", 3);
+                ok = 0;
             }
             if (recv == '\n') {
                 printf("\r\n");
@@ -34,10 +47,5 @@ int main() {
         }
         printf("%s\n",buf);
         RPC::call(buf, outbuf);
-        
-        // if((float)ping < 10) {
-        //     car.stop();
-        //     printf("distance = %f\n", (float)ping);
-        // }
     }
 }
