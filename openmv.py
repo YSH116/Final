18af42d5e2fbcdb7450650028ba8e6d75a162e76
YSH116 <74852896+YSH116@users.noreply.github.com>
2021-06-22 21:22:05 +0800

    Update openmv.py

--- openmv.py
+++ openmv.py
@@ -1,8 +1,8 @@
 import pyb, sensor, image, time, math
-enable_lens_corr = False # turn on for straighter lines...
+enable_lens_corr = False
 sensor.reset()
-sensor.set_pixformat(sensor.RGB565) # grayscale is faster
-sensor.set_framesize(sensor.QQVGA)
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QQVGA) # we run out of memory if the resolution is much bigger...
 sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
 sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
@@ -13,6 +13,7 @@ f_y = (2.8 / 2.952) * 120 # find_apriltags defaults to this if not set
 c_x = 160 * 0.5 # find_apriltags defaults to this if not set (the image.w * 0.5)
 c_y = 120 * 0.5 # find_apriltags defaults to this if not set (the image.h * 0.5)
 
+n = 1
 
 def degrees(radians):
    return (180 * radians) / math.pi
@@ -22,35 +23,13 @@ def cm(none):
 
 uart = pyb.UART(3,9600,timeout_char=1000)
 uart.init(9600,bits=8,parity = None, stop=1, timeout_char=1000)
-i = 0
 
 while(True):
-   clock.tick()
-   img = sensor.snapshot()
-   if enable_lens_corr: img.lens_corr(1.8) # for 2.8mm lens...
-   if i < 20:
-       for l in img.find_line_segments(roi = (80, 10, 40, 10), merge_distance = 200, max_theta_diff = 5):
-          img.draw_line(l.line(), color = (255, 0, 0))
-          line = (l.x1(), l.y1(), l.x2(), l.y2())
-          print("x1: %d, y1: %d, x2: %d, y2: %d" % line)
-          i+=1
-          if (l.x2() < 70) and i < 20:
-            uart.write(("/turn/run 40 -0.3 \n").encode())
-            time.sleep_ms(100)
-            print("right")
-          elif (l.x2() > 100) and i < 20:
-            uart.write(("/turn/run 40 0.3 \n").encode())
-            time.sleep_ms(100)
-            print("left")
-          elif i > 20:
-            break
-          else:
-            uart.write(("/goStraight/run 40 \n").encode())
-            time.sleep_ms(100)
-            print("Go straight")
-   #uart.write(("/stop/run \n").encode())
-   else:
-       for tag in img.find_apriltags(fx=f_x, fy=f_y, cx=c_x, cy=c_y): # defaults to TAG36H11
+    clock.tick()
+    img = sensor.snapshot()
+    if (img.find_apriltags()):
+        n = 0;
+        for tag in img.find_apriltags(fx=f_x, fy=f_y, cx=c_x, cy=c_y): # defaults to TAG36H11
           img.draw_rectangle(tag.rect(), color = (255, 0, 0))
           img.draw_cross(tag.cx(), tag.cy(), color = (0, 255, 0))
 
@@ -63,11 +42,32 @@ while(True):
           elif cm(tag.x_translation()) < 0 and abs(cm(tag.z_translation())) > 14:
             uart.write(("/turn/run 40 -0.3 \n").encode())
             print("r")
-          elif abs(cm(tag.z_translation())) < 14:
-            uart.write(("/stop/run \n").encode())
-            print("stop2\n")
-            uart.write(("K \n").encode())
-          else:
-            uart.write(("/goStraight/run 100 \n").encode())
+          elif abs(cm(tag.z_translation())) > 14:
+            uart.write(("/goStraight/run 40 \n").encode())
             print("g")
-       print("FPS %f" % clock.fps())
+          else:
+            uart.write(("/stop/run \n").encode())
+            print("stop\n")
+            uart.write(("K \n").encode())
+    else:
+        if enable_lens_corr: img.lens_corr(1.8) # for 2.8mm lens...
+        for l in img.find_line_segments(roi = (80, 10, 50, 20), merge_distance = 200, max_theta_diff = 5):
+          img.draw_line(l.line(), color = (255, 0, 0))
+          line = (l.x1(), l.y1(), l.x2(), l.y2(), l.theta())
+          print("x1: %d, y1: %d, x2: %d, y2: %d, theta: %d" % line)
+          if l and n:
+            if l.theta() > 10 and l.theta() < 60:
+                uart.write(("/turn/run 40 -0.3 \n").encode())
+                print("right")
+            elif l.theta() < 160 and l.theta() > 140:
+                uart.write(("/turn/run 40 0.3 \n").encode())
+                print("left")
+            else:
+                uart.write(("/goStraight/run 40 \n").encode())
+                print("Go straight")
+            time.sleep(0.05)
+          else:
+            uart.write(("/stop/run \n").encode())
+            print("stop\n")
+            time.sleep(0.05)
+
